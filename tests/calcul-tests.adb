5ab76c0bbbf7aa65c2d30b330cbb50a5005cdfeb
Marcel Hernandez <e6990620@gmail.com>
2020-07-04 22:01:50 +0200

    use a normal Test_Case instead of a Simple_Test_Case (allows more than one testing routine in the package)

--- tests/calcul-tests.adb
+++ tests/calcul-tests.adb
@@ -1,15 +1,28 @@
 with AUnit.Assertions;
 
 package body Calcul.Tests is
-   function Name (T : Test) return AUnit.Message_String is
-      pragma Unreferenced (T);
-   begin
-      return AUnit.Format ("Test Calcul package");
-   end Name;
-
-   procedure Run_Test (T : in out Test) is
+   procedure Test_Suma (T : in out AUnit.Test_Cases.Test_Case'Class) is
       pragma Unreferenced (T);
    begin
       AUnit.Assertions.Assert (Calcul.Suma(1, 2) = 3, "1 plus 2 is 3");
-   end Run_Test;
+   end Test_Suma;
+
+   procedure Test_Divisio (T : in out AUnit.Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
+   begin
+      AUnit.Assertions.Assert (Calcul.Divisio(1, 2) = 0.5, "1 divided by 2 is 0.5");
+   end Test_Divisio;
+
+   function Name (T : Test) return AUnit.Message_String is
+      pragma Unreferenced (T);
+   begin
+      return AUnit.Format ("Calcul Unit Tests");
+   end Name;
+
+   procedure Register_Tests (T: in out Test) is
+      use AUnit.Test_Cases.Registration;
+   begin
+      Register_Routine (T, Test_Suma'Access, "Test Suma function");
+      Register_Routine (T, Test_Divisio'Access, "Test Divisio function");
+   end Register_Tests;
 end Calcul.Tests;
--- tests/calcul-tests.ads
+++ tests/calcul-tests.ads
@@ -1,9 +1,9 @@
-with AUnit.Simple_Test_Cases;
+with AUnit.Test_Cases;
 
 package Calcul.Tests is
-   type Test is new AUnit.Simple_Test_Cases.Test_Case with null record;
+   type Test is new AUnit.Test_Cases.Test_Case with null record;
+
+   procedure Register_Tests (T: in out Test);
 
    function Name (T : Test) return AUnit.Message_String;
-
-   procedure Run_Test (T : in out Test);
 end Calcul.Tests;
