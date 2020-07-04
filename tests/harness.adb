1837d830712e6a1611ea68a7d629c8ce7ed627b2
Marcel Hernandez <e6990620@gmail.com>
2020-07-04 21:14:02 +0200

    set exit code dependening on test outcome

--- tests/harness.adb
+++ tests/harness.adb
@@ -1,10 +1,18 @@
+with Ada.Command_Line;
+with AUnit; use AUnit;
 with AUnit.Reporter.Text;
 with AUnit.Run;
 with La_Suite;
 
 procedure Harness is
-   procedure Runner is new AUnit.Run.Test_Runner (La_Suite.Suite);
+   function Runner is new AUnit.Run.Test_Runner_With_Status (La_Suite.Suite);
+
+   Result : AUnit.Status;
    Reporter : AUnit.Reporter.Text.Text_Reporter;
 begin
-   Runner (Reporter);
+   Result := Runner (Reporter);
+
+   if Result /= AUnit.Status'(Success) then
+      Ada.Command_Line.Set_Exit_Status(1);
+   end if;
 end Harness;
